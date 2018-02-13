e0007917ce92b712f209f8bdc34793a9e81c8ed4
Romil Parhwal <romilparhwal007@gmail.com>
2018-02-13 13:03:03 -0500

    Random Changes

--- MAD3004/Order.swift
+++ MAD3004/Order.swift
@@ -11,18 +11,19 @@ import Foundation
 class Order: ShoppingCart{
     var orderID: String?
     var address: String?
-    
-    fileprivate var dateOrdered: Date?
+    var itemCount: Int?
+    private var dateOrdered: Date?
     
     override init(){
         super.init()
     }
     
-    init(_ orderID: String,_ address: String,_ dateOrdered: Date,_ cartID: String,_ userID: String){
+    init(_ orderID: String,_ address: String,_ dateOrdered: Date,_ cartID: String,_ userID: String,_ itemCount: Int){
         self.orderID = orderID
         self.address = address
         self.dateOrdered = dateOrdered
         super.init(cartID, userID)
+        self.itemCount = super.countProductQuantity()
     }
     
     override init(_ orderID: String,_ address: String){
@@ -35,5 +36,6 @@ class Order: ShoppingCart{
         print("Order ID: \(orderID)")
         print("Address: \(address)")
         print("Date Ordered: \(dateOrdered)")
+        print("Item Count: \(itemCount)")
     }
 }
